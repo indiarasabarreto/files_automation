ae2b7d516affe0079b5c80f409d5453bad41cdc0
Indiara dos Santos Sá Barreto <66640743+indiarasabarreto@users.noreply.github.com>
2026-04-07 06:31:57 -0300

    Update file_organizer.py
    updating and trying better options to make the code more clean

--- files_automation/file_organizer.py
+++ files_automation/file_organizer.py
@@ -1,21 +1,49 @@
 import os
 import shutil
 
-file_organizer = 'files'
-destination_folder = 'organized_files'
 
-extensions = {
-    'images': ['.WebP', '.png'],
-    'documents': ['.pdf'],
-    'spreadsheets': ['.xlsx']
-}
+base_directory = os.path.dirname(os.path.abspath(__file__))
 
-for filename in os.listdir(file_organizer):
-    file_path = os.path.join(file_organizer, filename)
 
-    for paste, types in extensions.items():
-        if any(filename.endswith(ext) for ext in types):
-            destination_path = os.path.join(destination_folder, paste)
-            os.makedirs(destination_path, exist_ok=True)
-            shutil.move(file_path, os.path.join(destination_path, filename))
-            break
+""" 
+    source_folders = [
+    os.path.join(base_directory, './organized_files/documents'),
+    os.path.join(base_directory, './organized_files/spreadsheets'),
+    os.path.join(base_directory, './organized_files/images')
+] 
+
+
+destination_folder = os.path.join(base_directory, './files')
+
+for folder in source_folders:
+    if not os.path.exists(folder):
+        print(f"Source folder '{folder}' does not exist. Skipping.")
+        continue
+
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+
+        if os.path.isfile(file_path):
+            destination = os.path.join(destination_folder, filename)
+            os.makedirs(destination_folder, exist_ok=True)
+
+            shutil.move(file_path, os.path.join(destination_folder, filename))
+            print(f"Moved '{filename}' to '{destination_folder}'") """
+
+
+
+base_path = os.path.join(base_directory, 'organized_files')
+
+for folder in os.listdir(base_path):
+    folder_path = os.path.join(base_path, folder)
+
+    if os.path.isdir(folder_path):
+        for filename in os.listdir(folder_path):
+            file_path = os.path.join(folder_path, filename)
+
+            if os.path.isfile(file_path):
+                destination = os.path.join(base_directory, 'files')
+                os.makedirs(destination, exist_ok=True)
+
+                shutil.move(file_path, os.path.join(destination, filename))
+                print(f'Movido: {filename} → {destination}')
