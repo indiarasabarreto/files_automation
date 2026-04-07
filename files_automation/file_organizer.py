86b21aea8be1afeb45583df0653a576ed723c854
Indiara dos Santos Sá Barreto <66640743+indiarasabarreto@users.noreply.github.com>
2026-04-07 06:58:22 -0300

    Implement continuous file organization
    Added a loop to continuously check for files to move every minute.

--- files_automation/file_organizer.py
+++ files_automation/file_organizer.py
@@ -1,13 +1,18 @@
 import os
 import shutil
+import time
 
 
 base_directory = os.path.dirname(os.path.abspath(__file__))
 
 base_path = os.path.join(base_directory, 'organized_files')
 
-for folder in os.listdir(base_path):
-    folder_path = os.path.join(base_path, folder)
+
+while True:
+    print("Checking files...")
+
+    for folder in os.listdir(base_path):
+        folder_path = os.path.join(base_path, folder)
 
     if os.path.isdir(folder_path):
         for filename in os.listdir(folder_path):
@@ -19,3 +24,5 @@ for folder in os.listdir(base_path):
 
                 shutil.move(file_path, os.path.join(destination, filename))
                 print(f'Movido: {filename} to {destination}')
+    
+    time.sleep(60)
