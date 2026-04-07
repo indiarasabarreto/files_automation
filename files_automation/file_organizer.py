3cf737876500b5f704025f71ee25b872a261c2f3
Indiara dos Santos Sá Barreto <66640743+indiarasabarreto@users.noreply.github.com>
2026-04-07 06:38:19 -0300

    Add files via upload

--- files_automation/file_organizer.py
+++ files_automation/file_organizer.py
@@ -4,8 +4,34 @@ import shutil
 
 base_directory = os.path.dirname(os.path.abspath(__file__))
 
-base_path = os.path.join(base_directory, 'organized_files')
 
+source_folders = [
+    os.path.join(base_directory, './organized_files/documents'),
+    os.path.join(base_directory, './organized_files/spreadsheets'),
+    os.path.join(base_directory, './organized_files/images')
+] 
+
+
+destination_folder = os.path.join(base_directory, './files')
+
+for folder in source_folders:
+    if not os.path.exists(folder):
+        print(f"Source folder '{folder}' does not exist. Skipping.")
+        continue
+
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+
+        if os.path.isfile(file_path):
+            destination = os.path.join(destination_folder, filename)
+            os.makedirs(destination_folder, exist_ok=True)
+
+            shutil.move(file_path, os.path.join(destination_folder, filename))
+            print(f"Moved '{filename}' to '{destination_folder}'")
+
+
+"""
+base_path = os.path.join(base_directory, 'organized_files')
 
 for folder in os.listdir(base_path):
     folder_path = os.path.join(base_path, folder)
@@ -20,3 +46,4 @@ for folder in os.listdir(base_path):
 
                 shutil.move(file_path, os.path.join(destination, filename))
                 print(f'Movido: {filename} to {destination}')
+"""
